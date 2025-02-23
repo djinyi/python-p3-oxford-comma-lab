ff08e5367b40f8ee23448990f850a6142c8c0a43
Daisy Yi <daisyyi@rocketmail.com>
2025-02-23 15:53:30 -0700

    lab completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,16 @@
 def oxford_comma(items):
-    return None
+    if len(items)==2:
+        items[-1]= "and " + items[-1]
+        return " ".join(items)
+    if len(items)==1:
+        return items[0]
+    if len(items)>1:
+        last_element = items[-1]
+        if last_element:
+            items[-1] = "and " + items[-1]
+        else:
+            print("dafuq")
+    else:
+        print("nothin")    
+    print(", ".join(items))
+    return ", ".join(items)
